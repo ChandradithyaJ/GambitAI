fc1d1b26573b1e645f59083734ebf38affeef7b1
Chandradithya <chandradithya@gmail.com>
2024-03-26 13:23:21 +0530

    get_children function added

--- gambitAI/test.py
+++ gambitAI/test.py
@@ -8,19 +8,20 @@ class ChessNode:
         self.minimax = minimax # float value
         self.player = player # boolean, WHITE - TRUE, BLACK - FALSE
         self.depth = depth
+        self.children = get_children()
         
-    @property
-    def children(self):
-        childChessNodes = []
+    
+def get_children(self):
+    childChessNodes = []
         
-        if self.depth < K_PLY:
-            env = ChessEnvV2(opponent='none', initial_board=self.state['board'])
-            moves = env.possible_moves
-            actions = [env.move_to_action(move) for move in moves]
-            for action in actions:
-                new_state, reward, done, info = env.step(action)
-                childChessNodes.append(ChessNode(new_state, 1, not self.player, self.depth + 1))
-                initial_state = env.reset()
+    if self.depth < K_PLY:
+        env = ChessEnvV2(opponent='none', initial_board=self.state['board'])
+        moves = env.possible_moves
+        actions = [env.move_to_action(move) for move in moves]
+        for action in actions:
+            new_state, reward, done, info = env.step(action)
+            childChessNodes.append(ChessNode(new_state, 1, not self.player, self.depth + 1))
+            initial_state = env.reset()
             
-        return childChessNodes
+    return childChessNodes
      
